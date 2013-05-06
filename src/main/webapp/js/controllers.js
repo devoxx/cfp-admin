cfdbe3b2d3c2dbe9c153e2a9bab7e17346a2b8f9
ybonnel <ybonnel@gmail.com>
2013-05-06 14:40:15 +0200

    Skeletton

---
+++ src/main/webapp/js/controllers.js
@@ -0,0 +1,16 @@
+// Injecting rootScope only allowed in AppCtrl, because of handling routing errors
+app.controller('AppCtrl', function($rootScope, $route, $log) {
+    $rootScope.$on('$routeChangeError', function(event, current, previous, rejection) {
+        $log.debug('$routeChangeError:', event, current, previous, rejection);
+    });
+    $rootScope.$on('$routeChangeStart', function(event, current, previous, rejection) {
+        $log.debug('$routeChangeStart', event, current, previous, rejection);
+    });
+    $rootScope.$on('$routeChangeSuccess', function(event, current, previous, rejection) {
+        $log.debug('$routeChangeSuccess', event, current, previous, rejection);
+    });
+    $log.debug($route.routes)
+});
+
+app.controller('HomeCtrl', function($scope, $location) {
+});
